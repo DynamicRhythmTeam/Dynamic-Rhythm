79798f2002671929ee61c34397ca5451e9cb89dc
willie3d <www.willdoyle10@gmail.com>
2023-11-29 20:43:12 -0500

    Mug Diffusion Implementation (Part 1)

--- Assets/Scripts/SongLoad.cs
+++ Assets/Scripts/SongLoad.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using 
 public class SongLoad : MonoBehaviour
 {
-    public object[] notes = (0,1)
-    public object[] songInfo = new object[4];
-    songInfo[0] = "Title";
-    songInfo[1] = "Artist";
-    songInfo[2] = AudioSource song;
-    songInfo[3] = ArrayList notes = new ArrayList();
+//    public object[] notes = (0,1)
+//    public object[] songInfo = new object[4];
+//    songInfo[0] = "Title";
+//    songInfo[1] = "Artist";
+//    songInfo[2] = AudioSource song;
+//    songInfo[3] = ArrayList notes = new ArrayList();
 
     // Start is called before the first frame update
     void Start()
